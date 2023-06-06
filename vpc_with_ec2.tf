78ce8810c4feb935a75070b947f847e864e2f09b
Naresh9919 <110754714+naresh9919@users.noreply.github.com>
2023-06-06 21:56:55 +0530

    Update vpc_with_ec2.tf

--- vpc_with_ec2.tf
+++ vpc_with_ec2.tf
@@ -2,66 +2,76 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "demo-server" {
-  ami = "ami-0376ec8eacdf70aae"
-  key_name = "jenkinskey"
-  instance_type = "t2.small"
-  subnet_id = aws_subnet.demo_subnet.id
-  vpc_security_group_ids = [aws_security_group.demo_security_group.id]
-}
+resource "aws_instance" "Jenkins-instance" {
+  ami           = "ami-0607784b46cbe5816"
+  instance_type = "t2.micro"
+  key_name = "terraform"
+  subnet_id = aws_subnet.Demo-public_subent_01.id
+  vpc_security_group_ids = [aws_security_group.jenkins-sg.id]
 
-//create vpc
-resource "aws_vpc" "demo_vpc" {
-  cidr_block = "10.10.0.0/16"
-}
-
-//create subnet
-resource "aws_subnet" "demo_subnet" {
-  vpc_id     = aws_vpc.demo_vpc.id
-  cidr_block = "10.10.1.0/24"
-  availability_zone = "ap-south-1a"
-
-  tags = {
-    Name = "demo_subnet"
+ tags = {
+    Name = "Jenkins-instance"
   }
 }
 
-//create internet getway
-resource "aws_internet_gateway" "demo_igw" {
-  vpc_id = aws_vpc.demo_vpc.id
+# creating vpc
 
-  tags = {
-    Name = "demo_igw"
-  }
+resource "aws_vpc" "Demo-vpc" {
+    cidr_block = "10.1.0.0/16"
+    tags = {
+      Name = "Demo-vpc"
+    }
+  
 }
 
-//create route table
-resource "aws_route_table" "demo_route_table" {
-  vpc_id = aws_vpc.demo_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.demo_igw.id
-  }
-  tags = {
-    Name = "demo_route_table"
-  }
+resource "aws_subnet" "Demo-public_subent_01" {
+    vpc_id = "${aws_vpc.Demo-vpc.id}"
+    cidr_block = "10.1.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "ap-south-1a"
+    tags = {
+      Name = "Demo-public_subent_01"
+    }
+  
 }
 
-//associate subnet with route table
-resource "aws_route_table_association" "demo_route_table_association" {
-  subnet_id      = aws_subnet.demo_subnet.id
-  route_table_id = aws_route_table.demo_route_table.id
+//Creating a Internet Gateway
+
+resource "aws_internet_gateway" "Demo-igw" {
+    vpc_id = "${aws_vpc.Demo-vpc.id}"
+    tags = {
+      Name = "Demo-igw"
+    }
 }
 
-//create security group
-resource "aws_security_group" "demo_security_group" {
-  name        = "demo_security_group"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.demo_vpc.id
+// Create a route table 
+
+resource "aws_route_table" "Demo-public-rt" {
+    vpc_id = "${aws_vpc.Demo-vpc.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.Demo-igw.id}"
+    }
+    tags = {
+      Name = "Demo-public-rt"
+    }
+}
+
+// Associate subnet with routetable 
+
+resource "aws_route_table_association" "Demo-rta-public-subent-1" {
+    subnet_id = "${aws_subnet.Demo-public_subent_01.id}"
+    route_table_id = "${aws_route_table.Demo-public-rt.id}"
+  
+}
+
+// create a security group
+
+resource "aws_security_group" "jenkins-sg" {
+  name        = "jenkins-sg"
+  vpc_id      = aws_vpc.Demo-vpc.id
 
   ingress {
-    description      = "TLS from VPC"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
@@ -78,6 +88,6 @@ resource "aws_security_group" "demo_security_group" {
   }
 
   tags = {
-    Name = "demo_security_group"
+    Name = "jenkins-sg"
   }
 }
